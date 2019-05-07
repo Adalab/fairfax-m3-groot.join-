d7508479cb3cc6c86f46f393fd97da1128a9fad4
antonellam <antonella.michelli@gmail.com>
2019-05-07 20:48:08 +0200

    Create Header component

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import logo from "./images/awesome_cards_logo.svg";
+
+class Header extends Component {
+    render () {
+        const header = (
+            <header className="header__@@option">
+                <img className="header__image" src= {logo} alt="awesome cards logo"/>
+            </header>
+        );
+    
+        return header;
+    }
+};
+
+export default Header;
