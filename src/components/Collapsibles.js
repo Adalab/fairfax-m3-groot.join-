c747b38f49f6130dbed6d898ce11f3a7c72eb638
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-05-08 20:58:09 +0200

    add react.fragment to collapsibles and form

--- src/components/Collapsibles.js
+++ src/components/Collapsibles.js
@@ -1,19 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import FormDesign from './FormDesign';
 import FormFill from './FormFill';
 import FormShare from './FormShare';
+import { directive } from '@babel/types';
 
-
-class Collapsibles extends Component{
-    render(){
-        return(
-            <FormDesign />
-            <FormFill />
-            <FormShare />
-
-        )
-    }
+class Collapsibles extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <FormDesign />
+        <FormFill />
+        <FormShare />
+      </React.Fragment>
+    );
+  }
 }
-
 
 export default Collapsibles;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Collapsibles from './Collapsibles';
 
-class Form extends Component{
-    render(){
-        return(
-            <Collapsibles />  
-            <Collapsibles />  
-            <Collapsibles />  
-
-        )
-    }
+class Form extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Collapsibles />
+        <Collapsibles />
+        <Collapsibles />
+      </React.Fragment>
+    );
+  }
 }
 
 export default Form;
