3b8a4e96b3f8247462dc459f58dd241f41f73dcd
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-05-09 15:20:36 +0200

    fix routes components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
-import logo from "./images/awesome_cards_logo.svg";
+import React, { Component } from 'react';
+import logo from "./../images/awesome_cards_logo.svg";
 
 class Header extends Component {
-    render () {
+    render() {
         const header = (
             <header className="header__@@option">
-                <img className="header__image" src= {logo} alt="awesome cards logo"/>
+                <img className="header__image" src={logo} alt="awesome cards logo" />
             </header>
         );
-    
+
         return header;
     }
 };
