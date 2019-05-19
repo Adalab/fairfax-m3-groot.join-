d34e36ac2fbb1c5dd809f3795e0312d7f7d1b3c6
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-05-19 20:58:59 +0200

    add share twitter

--- src/components/FormShare.js
+++ src/components/FormShare.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import iconTwitter from './../images/twitter.png'
+
+const urlBack = 'http://urlBack.com';
+
+
 class FormShare extends Component {
     render() {
         return (
@@ -7,17 +12,44 @@ class FormShare extends Component {
                     <button className="button__create" type="button">
                         <i className="far fa-address-card form__card-icon"></i>
                         Crear tarjeta
-                </button>
-                    <div className="card__created hide">
+                     </button>
+
+                    <section
+                        class="align_share align_share_text"
+                    >
+                        <p class="card-create">La tarjeta ha sido creada</p>
+                        <p>
+                            <a class="card-link" href={urlBack}>{urlBack}</a>
+                        </p>
+                        <div class="align_share">
+                            <a
+                                class="twitter-share-button"
+                                href={'https://twitter.com/intent/tweet?text= Mirad%20que%20tarjeta%20más%20chachi%20he%20creado%20con%20la%20aplicación%20"Awesome%20Profile-cards"&url=' + urlBack}
+                                data-size="large"
+                                target="_blank"
+                            >
+                                <img
+                                    class="icon__twitter"
+                                    src={iconTwitter}
+                                    alt="compatir en Twitter"
+                                />Compartir en Twitter
+                            </a>
+                        </div>
+                    </section>
+
+
+
+
+                    {/* <div className="card__created hide">
                         <h2 className="form__share-subtitle">La tarjeta ha sido creada:</h2>
                         {/* <a href="" className="card__link" target="_blank"></a>
-                        <a className="share__twitter twitter-share-button-share" href="" target="_blank"> */}
-                            <i className="fab fa-twitter form__icons-twitter"></i>
-                            Compartir en twitter
-                    {/* </a> */}
-                    </div>
+                        <a className="share__twitter twitter-share-button-share" href="" target="_blank"> 
+                    <i className="fab fa-twitter form__icons-twitter"></i>
+                    Compartir en twitter
+                    </a> 
+                </div> */}
                 </div>
-            </fieldset>
+            </fieldset >
         )
     }
 }
---
+++ src/components/ShareTwitter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ShareTwitter extends Component {
+    render() {
+        return (
+            <h1>hola mundo</h1>
+        );
+    }
+}
+
+export default ShareTwitter;
