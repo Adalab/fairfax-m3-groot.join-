cc0c9868a3e235f2fa0959a13d77282718884cd2
antonellam <antonella.michelli@gmail.com>
2019-05-22 10:42:44 +0200

    Single-open collapsible

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,47 @@
-import React, { Component } from 'react';
-import Collapsibles from './Collapsibles';
-import FormDesign from './FormDesign';
-import FormFill from './FormFill';
-import FormShare from './FormShare';
+import React, { Component } from "react";
+import Collapsibles from "./Collapsibles";
+import FormDesign from "./FormDesign";
+import FormFill from "./FormFill";
+import FormShare from "./FormShare";
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.handleCollapsible = this.handleCollapsible.bind(this);
+    this.state = {
+      open: 'hide',
+    };
+  }
+
+  handleCollapsible(event) {
+    const name = event.currentTarget.getAttribute('name');
+    this.setState((prevState, props) => {
+      return {
+        open: prevState.open === name ? 'hide' : name,
+      };
+    });
+  }
 
   render() {
-
     return (
-      < form className="main-form" action="">
-
-        <Collapsibles name="Diseña" icon="far fa-object-ungroup form__icons">
+      <form className="main-form" action="">
+        <Collapsibles
+          name="Diseña"
+          icon="far fa-object-ungroup form__icons"
+          handleCollapsible={this.handleCollapsible}
+          open={this.state.open}
+        >
           <FormDesign
             palette={this.props.palette}
             handleChangeCard={this.props.handleChangeCard}
           />
-        </ Collapsibles >
-        <Collapsibles name="Rellena" icon="far fa-keyboard form__icons">
+        </Collapsibles>
+        <Collapsibles
+          name="Rellena"
+          icon="far fa-keyboard form__icons"
+          handleCollapsible={this.handleCollapsible}
+          open={this.state.open}
+        >
           <FormFill
             name={this.props.name}
             job={this.props.job}
@@ -28,16 +52,18 @@ class Form extends Component {
             handleChangeCard={this.props.handleChangeCard}
             getImage={this.props.getImage}
             photo={this.props.photo}
-
           />
-        </ Collapsibles >
-        <Collapsibles name="Comparte" icon="fas fa-share-alt form__icons">
+        </Collapsibles>
+        <Collapsibles
+          name="Comparte"
+          icon="fas fa-share-alt form__icons"
+          handleCollapsible={this.handleCollapsible}
+          open={this.state.open}
+        >
           <FormShare card={this.props} />
-        </Collapsibles >
-
+        </Collapsibles>
       </form>
-
-    )
+    );
   }
 }
 
