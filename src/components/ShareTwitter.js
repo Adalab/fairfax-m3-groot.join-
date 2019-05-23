b272ea8f23504828b4d2eb826e75ce331fddb8e2
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-05-23 20:35:27 +0200

    add urlError control

--- src/components/ShareTwitter.js
+++ src/components/ShareTwitter.js
@@ -5,15 +5,17 @@ import iconTwitter from './../images/twitter.png'
 
 class ShareTwitter extends Component {
     render() {
+        const hideTwitter = this.props.stateError ? 'hide' : ''
         const urlBack = this.props.stateUrl || this.props.stateError;
+
 
         return (
             <section className={`align_share align_share_text ${this.props.showTwitter}`}>
-                <p className="card-create">La tarjeta ha sido creada</p>
+                <p className={`card-create ${hideTwitter}`}>La tarjeta ha sido creada</p>
                 <p>
                     <a className="card-link" href={urlBack}>{urlBack}</a>
                 </p>
-                <div className="align_share">
+                <div className={`align_share ${hideTwitter}`}>
                     <a
                         className="twitter-share-button"
                         href={'https://twitter.com/intent/tweet?text= Mirad%20que%20tarjeta%20más%20chachi%20he%20creado%20con%20la%20aplicación%20"Awesome%20Profile-cards"&url=' + urlBack}
