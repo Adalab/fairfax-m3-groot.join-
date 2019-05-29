64ab601e81d6ec54b3ccd242bd7b468777718a5b
antonellam <antonella.michelli@gmail.com>
2019-05-29 18:44:08 +0200

    Create docs folder

---
+++ docs/precache-manifest.5d58eb451ebe1fdf43b24c4da994dcd0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6e095ddab2e050b5aa92f2f01547be",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1fb8455220ca59bce30e",
+    "url": "./static/css/main.0e9e3573.chunk.css"
+  },
+  {
+    "revision": "00cbfabc85e888108ccf",
+    "url": "./static/js/2.71a3ca81.chunk.js"
+  },
+  {
+    "revision": "1fb8455220ca59bce30e",
+    "url": "./static/js/main.36d1f5cc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "9889cace18b38106606a69d552c5d440",
+    "url": "./static/media/autumnopacity1.9889cace.jpg"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/awesome_cards_logo.0118e311.svg"
+  },
+  {
+    "revision": "12ad4d9a3cb790020082bdc7bb6c4af3",
+    "url": "./static/media/default-avatar.12ad4d9a.jpg"
+  },
+  {
+    "revision": "c0653f236e1ede7ad702e00d77df5768",
+    "url": "./static/media/greenopacity1.c0653f23.jpg"
+  },
+  {
+    "revision": "2076125f2240c5098e583b5a23a29417",
+    "url": "./static/media/groot.2076125f.jpeg"
+  },
+  {
+    "revision": "8b74954a47c6061ca75da37f56bd5b91",
+    "url": "./static/media/snow1opacity1.8b74954a.jpg"
+  }
+]);
